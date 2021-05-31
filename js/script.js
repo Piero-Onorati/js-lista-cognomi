0f5545f83dc7c9dc51361d4dd55ed13dbcad7533
Piero <piero.onorati@virgilio.it>
2021-05-31 14:04:21 +0200

    start project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Mail:
+1. Chiedi all’utente la sua email,
+2. Controlla che sia nella lista di chi può accedere, quindi stampa un messaggio appropriato sull’esito del controllo.*/
+
+
+/* Lista Cognomi:
+1. Chiedi all’utente il cognome ed inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+2. Stampa la lista ordinata alfabeticamente e scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova.*/ 
