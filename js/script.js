b8f01d1d6a94d73e3a86933be8183c2d2c34d3d9
Piero <piero.onorati@virgilio.it>
2021-05-31 14:18:11 +0200

    add algorithm for "Mail" exercise

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@
 1. Chiedi all’utente la sua email,
 2. Controlla che sia nella lista di chi può accedere, quindi stampa un messaggio appropriato sull’esito del controllo.*/
 
+var acceptedEmails = [ 'piero@gmail.com', 'pluto@gmail.com', 'pippo@gmail.com', 'topolino@gmail.com', 'paperino@gmail.com'];
+
+var nuovaEmail = prompt('Inserisci il tuo indirizzo mail');
+
+if (acceptedEmails.includes(nuovaEmail)) {
+    document.getElementById('mail-massage').innerHTML = 'Il tuo indirizzo mail è già presente nella lista' ;
+} else {
+    document.getElementById('mail-massage').innerHTML = 'Il tuo indirizzo mail è stato aggiunto correttamente alla lista' ;
+    
+}
+
 
 /* Lista Cognomi:
 1. Chiedi all’utente il cognome ed inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
